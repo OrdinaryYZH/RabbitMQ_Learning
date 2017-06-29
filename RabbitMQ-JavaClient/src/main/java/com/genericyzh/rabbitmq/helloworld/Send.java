22dc626f1099284c88b1e4db028ede5b29fdb588
genericyzh <yaozihuan@163.com>
2017-06-29 15:52:59 +0800

    【RabbitMQ最简单的发送接收消息】

---
+++ RabbitMQ-JavaClient/src/main/java/com/genericyzh/rabbitmq/helloworld/Send.java
@@ -0,0 +1,31 @@
+package com.genericyzh.rabbitmq.helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author genericyzh
+ * @date 2017/6/29 14:55
+ */
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
