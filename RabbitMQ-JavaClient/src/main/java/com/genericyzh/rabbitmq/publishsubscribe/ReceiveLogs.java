740b040639d9fd6feb91645866a3f296cdf6b57a
genericyzh <yaozihuan@163.com>
2017-06-30 15:39:52 +0800

    【RabbitMQ:订阅发布模式，交换机把消息发给所有队列】

---
+++ RabbitMQ-JavaClient/src/main/java/com/genericyzh/rabbitmq/publishsubscribe/ReceiveLogs.java
@@ -0,0 +1,36 @@
+package com.genericyzh.rabbitmq.publishsubscribe;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author genericyzh
+ * @date 2017/6/30 12:02
+ */
+public class ReceiveLogs {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+        String queue = channel.queueDeclare().getQueue(); // 得到一个临时队列
+        channel.queueBind(queue, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(queue, true, consumer);
+    }
+}
